02d5e5a24d379d494a6b8f9f99c921c6ebd69b92
draci1 <florijan.ibraimi@gmail.com>
2018-05-04 19:54:33 +0200

    Functionalized form for creating contracts
    CRUD: [Create]

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
-const DB_URI = "mongodb://localhost:27017/todoapp";
+const DB_URI = "mongodb://localhost:27017/Techstitution-ContractForm";
 const bodyParser = require('body-parser');
 const Router = express.Router();
 const ObjectId=require('mongodb').ObjectId;
@@ -24,39 +24,46 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/todo/add',(req,res,next) => {
-    todos.insert({title: req.body.title, description: req.body.description.trim()}, (err,document)=>{
-        if(err){
-            console.log(err);
-            return;
-        }
-        res.redirect('/');
-    })
-});
-
 app.get('/', function (req,res){
-    todos.find({}).toArray(function(err,docs){
-        if(err){
-            console.log(err);
-        }
-        res.render('index',{docs: docs});
-    })
-});
-
-app.get('/todo/:id', function(req,res){
-    console.log(req.params.id);
-    todos.findOne({_id:ObjectId(req.params.id)}, function(err,doc){
-        if(err){
-            console.log(err);
-        }
-        res.render('show',{doc:doc});
+        res.render('index');
     });
+
+app.post('/create',function(req,res,next){
+    MongoClient.connect(DB_URI, function(err, db) {
+        if(err) { throw err;  }
+        var collection = db.collection('contracts');
+        var contract = {
+            contract_title:req.body.contract_title,
+            contract_language:{
+                albanian: req.body.language1,
+                english: req.body.language2,
+                serbian: req.body.language3
+            },
+            procurement_number:req.body.procurement_number,
+            procurement_type:req.body.procurement_type,
+            procurement_activity:req.body.procurement_activity,
+            date_start:req.body.date_start,
+            date_publication:req.body.date_publication,
+            date_signed:req.body.date_signed,
+            imp_deadline_start:req.body.imp_deadline_start,
+            imp_deadline_end:req.body.imp_deadline_end,
+            conclusion_date:req.body.conclusion_date,
+            price:req.body.price,
+            total_price:req.body.total_price,
+            contractor_name:req.body.contractor_name
+         };
+        collection.insert(contract, function(err, result) {
+        if(err) { throw err; }
+          db.close();
+          res.redirect('/');
+      });
+    })
 });
 
 
 app.listen(3000, "localhost", (err) => {
     if(err){
-        console.log("Something's off with the connection [insert Kim Kardashian's tragic gif here]: " + err);
+        console.log("Something's off with the connection: " + err);
     }
     else{
         console.log("Listening to localhost!");
