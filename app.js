64fb293a8b6edbc2b7161a517340cf88eb3edc04
draci1 <florijan.ibraimi@gmail.com>
2018-05-06 21:08:42 +0200

    View for contracts
    Data to show yet undecided. Subject to changes.

--- app.js
+++ app.js
@@ -6,67 +6,72 @@ const MongoClient = require('mongodb').MongoClient;
 const DB_URI = "mongodb://localhost:27017/Techstitution-ContractForm";
 const bodyParser = require('body-parser');
 const Router = express.Router();
-const ObjectId=require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
-MongoClient.connect(DB_URI, (err,db) => {
+MongoClient.connect(DB_URI, (err, db) => {
     if (err) {
         console.log('Error connecting to db: ' + err);
         return;
     }
-    todos = db.collection('todos');
-console.log('Successful connection to db' + DB_URI);
+    contracts = db.collection('contracts');
+    console.log('Successful connection to db' + DB_URI);
 });
 
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req,res){
-        res.render('index');
-    });
+app.get('/', function (req, res) {
+    contracts.find({}).toArray(function(err,docs){
+        if(err){
+            console.log(err);
+        }
+        res.render('index',{docs: docs});
+    })
+});
 
-app.post('/create',function(req,res,next){
-    MongoClient.connect(DB_URI, function(err, db) {
-        if(err) { throw err;  }
+app.post('/create', function (req, res, next) {
+    MongoClient.connect(DB_URI, function (err, db) {
+        if (err) { throw err; }
         var collection = db.collection('contracts');
         var contract = {
-            contract_title:req.body.contract_title,
-            contract_language:{
+            contract_title: req.body.contract_title,
+            contract_language: {
                 albanian: req.body.language1,
                 english: req.body.language2,
                 serbian: req.body.language3
             },
-            procurement_number:req.body.procurement_number,
-            procurement_type:req.body.procurement_type,
-            procurement_activity:req.body.procurement_activity,
-            date_start:req.body.date_start,
-            date_publication:req.body.date_publication,
-            date_signed:req.body.date_signed,
-            imp_deadline_start:req.body.imp_deadline_start,
-            imp_deadline_end:req.body.imp_deadline_end,
-            conclusion_date:req.body.conclusion_date,
-            price:req.body.price,
-            total_price:req.body.total_price,
-            contractor_name:req.body.contractor_name
-         };
-        collection.insert(contract, function(err, result) {
-        if(err) { throw err; }
-          db.close();
-          res.redirect('/');
-      });
-    })
+            procurement_number: req.body.procurement_number,
+            procurement_type: req.body.procurement_type,
+            procurement_activity: req.body.procurement_activity,
+            date_start: req.body.date_start,
+            date_publication: req.body.date_publication,
+            date_signed: req.body.date_signed,
+            imp_deadline_start: req.body.imp_deadline_start,
+            imp_deadline_end: req.body.imp_deadline_end,
+            conclusion_date: req.body.conclusion_date,
+            price: req.body.price,
+            total_price: req.body.total_price,
+            contractor_name: req.body.contractor_name
+        };
+        collection.insert(contract, function (err, result) {
+            if (err) { throw err; }
+            db.close();
+            res.redirect('/');
+        });
+    });
 });
 
 
 app.listen(3000, "localhost", (err) => {
-    if(err){
+    if (err) {
         console.log("Something's off with the connection: " + err);
     }
-    else{
+    else {
         console.log("Listening to localhost!");
-}
+    }
 });
 
--- public/js/popup.js
+++
@@ -1,5 +0,0 @@
-function popup(id,title,description,specialization){
-    document.getElementById(id).style.display='block';
-    document.getElementById('projtitlemodal').innerHTML=title + " | " + specialization;
-    document.getElementById('projcontentmodal').innerHTML=description;
-}
