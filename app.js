b4c4839d0d719909d6777bf6b3fabda6f2195b66
draci1 <florijan.ibraimi@gmail.com>
2018-05-09 19:13:11 +0200

    attempt #1 for edit

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    contracts.find({}).toArray(function(err,docs){
-        if(err){
+    contracts.find({}).toArray(function (err, docs) {
+        if (err) {
             console.log(err);
         }
-        res.render('index',{docs: docs});
+        res.render('index', { docs: docs });
     })
 });
 
@@ -59,6 +59,42 @@ app.post('/create', function (req, res, next) {
         };
         collection.insert(contract, function (err, result) {
             if (err) { throw err; }
+            db.close();
+            res.redirect('/');
+        });
+    });
+});
+
+app.post('/edit', function (req, res, next) {
+    MongoClient.connect(DB_URI, function (err, db) {
+        if (err) { throw err; }
+        var collection = db.collection('contracts');
+        var query = { _id: req.body.edit_hidden_id };
+        var newval = {
+            $set:
+                {
+                    contract_title: req.body.edit_contract_title,
+                    contract_language: {
+                        albanian: req.body.edit_language1,
+                        english: req.body.edit_language2,
+                        serbian: req.body.edit_language3
+                    },
+                    procurement_number: req.body.edit_procurement_number,
+                    procurement_activity: req.body.edit_procurement_activity,
+                    date_start: req.body.edit_date_start,
+                    date_publication: req.body.edit_date_publication,
+                    date_signed: req.body.edit_date_signed,
+                    imp_deadline_start: req.body.edit_imp_deadline_start,
+                    imp_deadline_end: req.body.edit_imp_deadline_end,
+                    conclusion_date: req.body.edit_conclusion_date,
+                    price: req.body.edit_price,
+                    total_price: req.body.edit_total_price,
+                    contractor_name: req.body.edit_contractor_name
+                }
+        };
+        collection.updateOne(query, newval, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
             db.close();
             res.redirect('/');
         });
